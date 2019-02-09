8fcd5bdf7b8681788a7e9e4a9b569e03b52bef0c
龙善敏 <longsm@mingdutech.com>
2019-02-09 20:38:10 +0800

    1. 使用配置设置druid，而不是代码

--- src/main/java/com/xsh/blog/config/DruidConfiguration.java
+++ src/main/java/com/xsh/blog/config/DruidConfiguration.java
@@ -1,60 +1,60 @@
-package com.xsh.blog.config;
-
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-@Slf4j
-public class DruidConfiguration {
-    private static final String DB_PREFIX = "spring.datasource";
-
-    private static ServletRegistrationBean servletRegistrationBean = null;
-
-    //读取相关的属性配置
-    @ConfigurationProperties(prefix = DB_PREFIX)
-    @Bean
-    public DataSource dataSource(){
-        return new DruidDataSource();
-    }
-
-    //配置一个管理后台的Servlet,配置Druid的监控
-    @Bean
-    public ServletRegistrationBean statViewServlet(){
-        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-        Map<String,String> initParams = new HashMap<String, String>();
-        initParams.put("loginUsername","admin");
-        initParams.put("loginPassword","feverwind");
-
-        bean.setInitParameters(initParams);
-        servletRegistrationBean = bean;
-        return bean;
-    }
-
-    //配置一个web监控的filter
-    @Bean
-    public FilterRegistrationBean webStatFilter(){
-        FilterRegistrationBean bean = new FilterRegistrationBean();
-        bean.setFilter(new WebStatFilter());
-
-        Map<String,String> initParams = new HashMap<>();
-        initParams.put("exclusions","*.js,*.css,*.gif,*.jpg,*.png,*.ico,/druid/*");
-
-        bean.setInitParameters(initParams);
-
-        bean.setUrlPatterns(Arrays.asList("/*"));
-
-        return  bean;
-    }
-}
+//package com.xsh.blog.config;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//import com.alibaba.druid.support.http.StatViewServlet;
+//import com.alibaba.druid.support.http.WebStatFilter;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//@Slf4j
+//public class DruidConfiguration {
+//    private static final String DB_PREFIX = "spring.datasource";
+//
+//    private static ServletRegistrationBean servletRegistrationBean = null;
+//
+//    //读取相关的属性配置
+//    @ConfigurationProperties(prefix = DB_PREFIX)
+//    @Bean
+//    public DataSource dataSource(){
+//        return new DruidDataSource();
+//    }
+//
+//    //配置一个管理后台的Servlet,配置Druid的监控
+//    @Bean
+//    public ServletRegistrationBean statViewServlet(){
+//        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+//        Map<String,String> initParams = new HashMap<String, String>();
+//        initParams.put("loginUsername","admin");
+//        initParams.put("loginPassword","feverwind");
+//
+//        bean.setInitParameters(initParams);
+//        servletRegistrationBean = bean;
+//        return bean;
+//    }
+//
+//    //配置一个web监控的filter
+//    @Bean
+//    public FilterRegistrationBean webStatFilter(){
+//        FilterRegistrationBean bean = new FilterRegistrationBean();
+//        bean.setFilter(new WebStatFilter());
+//
+//        Map<String,String> initParams = new HashMap<>();
+//        initParams.put("exclusions","*.js,*.css,*.gif,*.jpg,*.png,*.ico,/druid/*");
+//
+//        bean.setInitParameters(initParams);
+//
+//        bean.setUrlPatterns(Arrays.asList("/*"));
+//
+//        return  bean;
+//    }
+//}
